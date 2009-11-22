ec3b176d07049607268dad1ec0df28cb3b2ab313
Adam Simpkins <adam@adamsimpkins.net>
2009-11-22 12:58:03 -0800

    mb: add parse_raw_results()

--- src/amass/mb/query.py
+++ src/amass/mb/query.py
@@ -2,8 +2,11 @@
 #
 # Copyright (c) 2009, Adam Simpkins
 #
+import io
+
 import musicbrainz2.disc as mbdisc
 import musicbrainz2.webservice as mbws
+import musicbrainz2.wsxml as mbwsxml
 
 from . import disc_id as mb_did
 
@@ -21,6 +24,7 @@ def query_disc_id(disc_id):
 
     return results
 
+
 def query_disc_id_raw(disc_id):
     ws = mbws.WebService()
 
@@ -37,6 +41,22 @@ def query_disc_id_raw(disc_id):
 
     buf = stream.read()
     return buf
+
+
+def parse_raw_results(results):
+    if isinstance(results, (file, io.IOBase)):
+        stream = results
+    elif isinstance(results, basestring):
+        stream = io.StringIO(results)
+
+    parser = mbwsxml.MbXmlParser()
+    try:
+        results = parser.parse(stream)
+    except mbwsxml.ParseError, ex:
+        # FIXME
+        raise
+
+    return results.getReleaseResults()
 
 
 def query_toc(toc):
