cab28d95cbb34aa3056654eaaeb07bed560b5c63
Adam Simpkins <adam@adamsimpkins.net>
2010-02-14 14:56:01 -0800

    rename: move find_track_files() into amass.archive
    This will allow it to be re-used in other programs.

--- src/rename.py
+++ src/rename.py
@@ -4,76 +4,15 @@
 #
 import optparse
 import os
-import re
 import sys
 
 from amass import archive
 from amass import metadata
 
 
-class FileInfo(object):
-    def __init__(self, path, metadata):
-        self.path = path
-        self.metadata = metadata
-
-    @property
-    def name(self):
-        return os.path.basename(self.path)
-
-
-def find_track_files(dir, suffix, tracks):
-    # Find all files with a matching suffix
-    files = []
-    for entry in os.listdir(dir):
-        if not entry.endswith(suffix):
-            continue
-        files.append(entry)
-
-    # Try to determine which file corresponds to which track
-    #
-    # We currently expect an exact 1:1 mapping
-    #
-    # TODO: At the moment, we look only at the filename.
-    # We should also support looking at the file metadata, if present
-    numbers_to_name = {}
-    paths_to_track = {}
-    for filename in files:
-        # Find all numbers in the filename
-        numbers = [int(n) for n in re.findall(r'\d+', filename)]
-        # Prune out numbers that aren't in the expected track number range
-        numbers = [n for n in numbers if 0 < n and n <= len(tracks)]
-
-        # TODO: For now, we require there to be exactly 1 number in each name.
-        # This certainly won't be good enough in the future.  We should detect
-        # patterns in the names, to try and figure out where the track number
-        # is.  (e.g., the number always comes at the very beginning, or always
-        # comes after the artist name, etc.)
-        if not numbers:
-            raise Exception('no track number found in filename %r' %
-                            (filename,))
-        if len(numbers) != 1:
-            raise Exception('multiple possible track numbers found in '
-                            'filename %r: %s' % (filename, numbers))
-
-        # Make sure there are no duplicates
-        number = numbers[0]
-        if numbers_to_name.has_key(number):
-            raise Exception('multiple files found for track number %d: '
-                            '%r and %r' %
-                            (number, numbers_to_name[number], filename))
-
-        numbers_to_name[number] = filename
-
-        path = os.path.join(dir, filename)
-        track = tracks[number - 1]
-        paths_to_track[path] = track
-
-    return paths_to_track
-
-
-def get_track_name(metadata):
-    return '%02d - %s' % (metadata.number, metadata.trackTitle)
-    # return 'track%02d' % (metadata.number,)
+def get_track_name(track):
+    return '%02d - %s' % (track.number, track.trackTitle)
+    # return 'track%02d' % (track.number,)
 
 
 def rename_files(info_list):
@@ -121,13 +60,7 @@ def main(argv):
     tracks = metadata.track.read(f)
     f.close()
 
-    suffix = '.wav'
-    file_map = find_track_files(dir.getWavDir(), suffix, tracks)
-    # Convert to a list of TrackInfo objects, sorted by track number
-    info_list = [FileInfo(path, track)
-                 for (path, track) in file_map.iteritems()]
-    info_list.sort(key = lambda t: t.metadata.number)
-
+    info_list = archive.find_track_files(dir.getWavDir(), '.wav', tracks)
     rename_files(info_list)
 
 
