ae32f2259fd59a1178e19e481c458bd906724a63
Adam Simpkins <adam@adamsimpkins.net>
2010-02-17 23:03:43 -0800

    archive: split directory layout code from AlbumDir
    Created a new DirLayout class, to manage the directory layout
    configuration independently from the main AlbumDir functionality.

--- src/amass/archive/album_dir.py
+++ src/amass/archive/album_dir.py
@@ -13,13 +13,83 @@ from .. import mb
 
 class AlbumDir(object):
     def __init__(self, path, new=False):
-        self.path = path
-
+        self.layout = DirLayout(path)
         if not new:
             # Check for a table of contents file,
             # to verify that this looks like an album directory
             if not os.path.isfile(self.getTocPath()):
                 raise NotAnAlbumDirError(self.path)
+
+    def getToc(self):
+        toc_path = self.layout.getTocPath()
+        try:
+            toc_file = open(toc_path, 'rb')
+        except IOError, ex:
+            if ex.errno == errno.ENOENT:
+                raise NotAnAlbumDirError(self.layout.path)
+            raise
+
+        toc_buf = toc_file.read()
+        toc_file.close()
+        return cdrom.FullTOC(toc_buf)
+
+    def getCddbEntries(self):
+        cddb_dir = self.layout.getCddbDir()
+        if not os.path.isdir(cddb_dir):
+            return None
+
+        entries = []
+        for entry in os.listdir(cddb_dir):
+            full_path = os.path.join(cddb_dir, entry)
+
+            cddb_file = io.open(full_path, 'r', encoding='UTF-8')
+            cddb_buf = cddb_file.read()
+            cddb_file.close()
+
+            data = cddb.data.parse(cddb_buf)
+            entries.append(data)
+
+        return entries
+
+    def getMbReleases(self):
+        try:
+            releases_file = open(self.layout.getMusicBrainzPath(), 'rb')
+        except IOError, ex:
+            if ex.errno == errno.ENOENT:
+                return None
+            raise
+
+        release_results = mb.parse_raw_results(releases_file)
+        releases_file.close()
+
+        return release_results
+
+    def getCdText(self):
+        try:
+            cdtext_file = open(self.layout.getCdTextPath(), 'rb')
+            cdtext_buf = cdtext_file.read()
+            cdtext_file.close()
+        except IOError, ex:
+            if ex.errno == errno.ENOENT:
+                return None
+            raise
+
+        cdtext = cdrom.cdtext.parse(cdtext_buf)
+        return cdtext
+
+
+class DirLayout(object):
+    """
+    Defines the layout of an album directory.
+
+    This object contains only path information.  It consolidates the directory
+    layout in a single location, so that this information is kept separate from
+    the rest of the logic.  This makes it easier if we later decide to alter
+    the directory layout, or support multiple or configurable directory
+    layouts.
+    """
+    def __init__(self, path):
+        self.path = path
 
     def getMetadataDir(self):
         return os.path.join(self.path, 'metadata')
@@ -53,60 +123,3 @@ class AlbumDir(object):
 
     def getFlacDir(self):
         return os.path.join(self.path, 'flac')
-
-    def getToc(self):
-        toc_path = self.getTocPath()
-        try:
-            toc_file = open(toc_path, 'rb')
-        except IOError, ex:
-            if ex.errno == errno.ENOENT:
-                raise NotAnAlbumDirError(self.path)
-            raise
-
-        toc_buf = toc_file.read()
-        toc_file.close()
-        return cdrom.FullTOC(toc_buf)
-
-    def getCddbEntries(self):
-        cddb_dir = self.getCddbDir()
-        if not os.path.isdir(cddb_dir):
-            return None
-
-        entries = []
-        for entry in os.listdir(cddb_dir):
-            full_path = os.path.join(cddb_dir, entry)
-
-            cddb_file = io.open(full_path, 'r', encoding='UTF-8')
-            cddb_buf = cddb_file.read()
-            cddb_file.close()
-
-            data = cddb.data.parse(cddb_buf)
-            entries.append(data)
-
-        return entries
-
-    def getMbReleases(self):
-        try:
-            releases_file = open(self.getMusicBrainzPath(), 'rb')
-        except IOError, ex:
-            if ex.errno == errno.ENOENT:
-                return None
-            raise
-
-        release_results = mb.parse_raw_results(releases_file)
-        releases_file.close()
-
-        return release_results
-
-    def getCdText(self):
-        try:
-            cdtext_file = open(self.getCdTextPath(), 'rb')
-            cdtext_buf = cdtext_file.read()
-            cdtext_file.close()
-        except IOError, ex:
-            if ex.errno == errno.ENOENT:
-                return None
-            raise
-
-        cdtext = cdrom.cdtext.parse(cdtext_buf)
-        return cdtext
